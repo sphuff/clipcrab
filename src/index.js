c9388761dcf72f923b1b6fbc306c8f564f7e305c
Stephen <farrenzo47@gmail.com>
2020-10-11 10:26:29 -0400

    Actually render home route

--- src/index.js
+++ src/index.js
@@ -87,10 +87,10 @@ server.get('/auth0-logo', (req, res) => {
 
 // weird but secure index.html and let rest through
 server.get('/', secured(), (req, res) => {
-  path.join(__dirname, '../', 'client/build/index.html')
+  res.sendFile(path.join(__dirname, '../', 'client/build/index.html'));
 });
 server.get('/index.html', secured(), (req, res) => {
-  path.join(__dirname, '../', 'client/build/index.html')
+  res.sendFile(path.join(__dirname, '../', 'client/build/index.html'));
 });
 
 server.use(express.static(path.join(__dirname, '../', 'client/build')));
