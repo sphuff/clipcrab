d7be3d8cf986f76801f0390f719e7d9774c45b5b
Stephen <farrenzo47@gmail.com>
2020-10-12 19:53:25 -0400

    Turn off 16:9 aspect ratio while working out bug

--- client/src/components/AspectRatioSelector/index.tsx
+++ client/src/components/AspectRatioSelector/index.tsx
@@ -8,7 +8,6 @@ export default function AspectRatioSelector({ onSelectAspectRatio, aspectRatio }
         <div>
             <label htmlFor="aspect-ratio-select">Aspect Ratio:</label>
             <select id='aspect-ratio-select' className='border mx-2 px-2 py-1 rounded shadow-lg' onChange={onSelect} defaultValue={aspectRatio}>
-                <option value='phone'>16:9</option>
                 <option value='square'>1:1</option>
             </select>
         </div>
