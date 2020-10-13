bb06803513d11d27de266b7386929755c44aa908
Stephen <farrenzo47@gmail.com>
2020-10-12 20:05:25 -0400

    Hopefully fix 16:9 issue

--- scripts/encode.sh
+++ scripts/encode.sh
@@ -2,5 +2,5 @@
 # ffmpeg -i src/files/video.mp4 -c:v libx264 src/files/video264.mp4
 
 # This is with vp9 (chrome only codec)
-ffmpeg -i ../src/files/video.mp4 -i ../src/files/mmbam.mp3 -c:v copy -c:a aac ../output.mp4
+ffmpeg -i https://podcast-clipper.s3.amazonaws.com/uploads/mmbammp3-2020-10-12T235713533Z.mp4 -i https://podcast-clipper.s3.amazonaws.com/uploads/mmbam.mp3 -y -c:v libx264 -preset fast -c:a aac output.mp4
 # ffmpeg -i output.webm -preset superfast output.mp4
