3b171cf4625acb7af4a9c9ed89d05a1cda64b010
Stephen <farrenzo47@gmail.com>
2020-10-27 09:50:57 -0400

    Mobile color picker styling

--- client/src/components/ColorPicker/index.jsx
+++ client/src/components/ColorPicker/index.jsx
@@ -2,22 +2,22 @@ import React, { useState } from 'react';
 import { SketchPicker } from 'react-color';
 
 export default function ColorPicker({ onColorSelect, hexColor }) {
-    const [color, setColor] = useState(hexColor);
+    const [color, setColor] = useState(`#${Number(hexColor).toString(16)}`);
     const [showSketchPicker, setShowSketchPicker] = useState(false);
     const handleChangeComplete = (color) => {
         // "#b22323" to 0xb22323
-        onColorSelect(color.hex.replace('#', '0x'));
         setColor(color.hex);
+        onColorSelect(color.hex.replace('#', '0x'));
     };
     return (
         <div className='relative'>
-            <div className='block lg:hidden w-16 h-16 rounded'
+            <div className='block lg:hidden w-16 h-8 rounded'
                 onMouseOver={() => setShowSketchPicker(true)}
                 onMouseOut={() => setShowSketchPicker(false)}
             >
                 <div className='p-4 rounded w-full h-full' style={{backgroundColor: color}}></div>
             </div>
-            <div className='lg:block'>
+            <div className='hidden lg:block'>
                 <SketchPicker onChangeComplete={ handleChangeComplete } color={color}/>
             </div>
             { showSketchPicker && (
